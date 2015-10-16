d5fedb959ad848ec76d0771e2f42821bee470114
dpg-daniela <daniela.giannocaro@chicagobooth.edu>
2015-10-15 20:09:20 -0500

    HW3 - Meteorologist

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -17,17 +17,33 @@ class MeteorologistController < ApplicationController
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
 
+    url_1 = "https://maps.googleapis.com/maps/api/geocode/json?address="+url_safe_street_address
+
+    require 'json'
+    
+    parsed_data_1 = JSON.parse(open(url_1).read)
+    latitude = parsed_data_1["results"][0]["geometry"]["location"]["lat"]
+    longitude = parsed_data_1["results"][0]["geometry"]["location"]["lng"]
+
+    @latitude = latitude.to_s
+    @longitude = longitude.to_s
 
 
-    @current_temperature = "Replace this string with your answer."
+    url_2 = "https://api.forecast.io/forecast/f75e23fbc60dd60d1695b54d6c7b4314/"+@latitude+","+@longitude
 
-    @current_summary = "Replace this string with your answer."
+    require 'json'
+    parsed_data_2 = JSON.parse(open(url_2).read)
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @current_temperature = parsed_data_2["currently"]["temperature"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @current_summary = parsed_data_2["currently"]["summary"]
+
+    @summary_of_next_sixty_minutes = parsed_data_2["minutely"]["summary"]
+
+    @summary_of_next_several_hours = parsed_data_2["hourly"]["summary"]
+
+    @summary_of_next_several_days = parsed_data_2["daily"]["summary"]
 
     render("street_to_weather.html.erb")
   end
